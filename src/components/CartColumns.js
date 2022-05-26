fca9f7d0301d5f28dbe6c3217a7d624964056187
nouria chaouy <nouria.chaouy@gmail.com>
2022-05-26 17:40:56 +0100

    finished cart components and added cart functions

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -3,15 +3,15 @@ import "./CartColumns.css";
 
 const CartColumns = () => {
   return (
-    <main>
-      <div className="cart-content cart-columns-section">
+    <main className="cart-columns-section">
+      <div className="cart-columns-content ">
         <h5>item</h5>
         <h5>price</h5>
         <h5>quantity</h5>
         <h5>subtotal</h5>
         <span></span>
       </div>
-      <hr />
+      <hr  className="cart-columns-hr"/>
     </main>
   );
 };
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./CartItem.css";
+import { formatPrice } from "../utils";
+import AmountButtons from "./AmountButtons";
+import { FaTrash } from "react-icons/fa";
+import { useCartContext } from "../context/cart_context";
 
-const CartItem = () => {
+const CartItem = ({ id, name, image, color, price, amount }) => {
+  const { removeItem, toggleAmount } = useCartContext();
+
+  const increase = () => {};
+  const decrease = () => {};
+
   return (
-    <div>CartItem</div>
-  )
-}
+    <main className="cart-item-section">
+      <div className="cart-item-title">
+        <img src={image} alt={name} className="cart-item-img" />
+        <div>
+          <h5 className="cart-item-name">{name}</h5>
+          <p className="cart-item-color">
+            color: <span style={{ background: color }}></span>
+          </p>
+          <h5 className="cart-item-price-small">{formatPrice(price)}</h5>
+        </div>
+      </div>
+      <h5 className="cart-item-price">{formatPrice(price)}</h5>
+      <AmountButtons
+        amount={amount}
+        increase={increase}
+        decrease={decrease}
+        className="cart-item-amount-btns"
+      />
+      <h5 className="cart-item-subtotal">{formatPrice(price * amount)}</h5>
+      <button
+        type="button"
+        className="cart-item-remove-btn"
+        onClick={() => removeItem(id)}
+      >
+        <FaTrash />
+      </button>
+    </main>
+  );
+};
 
-export default CartItem
+export default CartItem;
